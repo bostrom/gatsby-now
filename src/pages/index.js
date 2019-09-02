f269edbe4bf5a01dadf1c8f48cee4e6891a70cab
Fredrik Boström <fredrik@fredrikbostrom.net>
2019-09-02 11:24:34 +0300

    Add api function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+function IndexPage() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    async function getData() {
+      const req = await fetch("/api/test")
+      const payload = await req.text()
+      setData(payload)
+    }
+    getData()
+  }, [])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Check out this data:</p>
+      {data}
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
